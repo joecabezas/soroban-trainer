98f839ed10049b15f758e0467d4a917b5d7d7cbe
Joe Cabezas Campos <joe.cabezas@gmail.com>
2020-05-25 00:08:15 -0700

    layout working

--- src/app.js
+++ src/app.js
@@ -1,100 +1,143 @@
-import { hot } from 'react-hot-loader/root';
+import clsx from 'clsx';
 import React from 'react';
+import {hot} from 'react-hot-loader/root';
 
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import Drawer from '@material-ui/core/Drawer';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import Divider from '@material-ui/core/Divider';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import FunctionsIcon from '@material-ui/icons/Functions';
 import ClearIcon from '@material-ui/icons/Clear';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import FunctionsIcon from '@material-ui/icons/Functions';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import MenuIcon from '@material-ui/icons/Menu';
+import MenuSection from './menu_section';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import ViewComfyIcon from '@material-ui/icons/ViewComfy';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import Collapse from '@material-ui/core/Collapse';
+import {makeStyles} from '@material-ui/core/styles';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+}));
+
+const sections = [
+  {
+    icon: <FunctionsIcon />,
+    label: 'Summatory',
+    content: null,
+  },
+  {
+    icon: <ClearIcon />,
+    label: 'Multiplications',
+    content: null,
+  },
+  {
+    icon: <ViewComfyIcon />,
+    label: 'Divisions',
+    content: null,
+  },
+];
 
 function App() {
-  const [isOpen, setIsOpen] = React.useState(false);
+  const classes = useStyles();
 
-  const handleDrawerOpen = () => {
-    setIsOpen(true);
-  };
+  const [open, setOpen] = React.useState(false);
 
-  const handleDrawerClose = () => {
-    setIsOpen(false);
-  };
+  function handleDrawerOpen() {
+    setOpen(true);
+  }
 
-  const [open, setOpen] = React.useState(true);
-
-  const handleClick = () => {
-    setOpen(!open);
-  };
+  function handleDrawerClose() {
+    setOpen(false);
+  }
 
   return (
-    <React.Fragment key={'left'}>
+    <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="static">
-        <Toolbar>
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar disableGutters={!open}>
           <IconButton
-            edge="start"
             color="inherit"
+            aria-label="Open drawer"
             onClick={handleDrawerOpen}
+            className={clsx(classes.menuButton, open && classes.hide)}
           >
             <MenuIcon />
-          </IconButton
-          >
+          </IconButton>
           <Typography variant="h6">
             Soroban Trainer
           </Typography>
         </Toolbar>
       </AppBar>
-      <Drawer anchor={'left'} open={isOpen}>
-        <IconButton onClick={handleDrawerClose}>
-          <ChevronLeftIcon />
-        </IconButton>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
         <Divider />
         <List component="nav">
-          <ListItem button>
-            <ListItemIcon>
-              <FunctionsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Summatory" />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <ClearIcon />
-            </ListItemIcon>
-            <ListItemText primary="Multiplications" />
-          </ListItem>
-          <ListItem button onClick={handleClick}>
-            <ListItemIcon>
-              <ViewComfyIcon />
-            </ListItemIcon>
-            <ListItemText primary="Divisions" />
-            {open ? <ExpandLess /> : <ExpandMore />}
-          </ListItem>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <List component="div" disablePadding>
-              <ListItem button>
-                <ListItemIcon>
-                  <StarBorder />
-                </ListItemIcon>
-                <ListItemText primary="Starred" />
-              </ListItem>
-            </List>
-          </Collapse>
+          {sections.map(
+            (section) => <MenuSection key={section.label} {...section} />,
+          )}
         </List>
+        <Divider />
       </Drawer>
-    </React.Fragment>
+    </div>
   );
 }
 
---
+++ src/menu_section.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+function MenuSection(props) {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <React.Fragment>
+      <ListItem button onClick={handleClick}>
+        <ListItemIcon>
+          {props.icon}
+        </ListItemIcon>
+        <ListItemText primary={props.label} />
+        {isOpen ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={isOpen} timeout="auto" unmountOnExit>
+        {props.content}
+      </Collapse>
+    </React.Fragment>
+  );
+}
+
+export default MenuSection;
