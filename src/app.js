a53d6c4be9cf3392aeb8eb5e09be74a52cf8ab99
Joe Cabezas Campos <joe.cabezas@gmail.com>
2020-05-24 19:37:48 -0700

    added eslint

--- src/app.js
+++ src/app.js
@@ -10,6 +10,17 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import Divider from '@material-ui/core/Divider';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import FunctionsIcon from '@material-ui/icons/Functions';
+import ClearIcon from '@material-ui/icons/Clear';
+import ViewComfyIcon from '@material-ui/icons/ViewComfy';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import Collapse from '@material-ui/core/Collapse';
 
 function App() {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -22,8 +33,15 @@ function App() {
     setIsOpen(false);
   };
 
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
     <React.Fragment key={'left'}>
+      <CssBaseline />
       <AppBar position="static">
         <Toolbar>
           <IconButton
@@ -44,6 +62,37 @@ function App() {
           <ChevronLeftIcon />
         </IconButton>
         <Divider />
+        <List component="nav">
+          <ListItem button>
+            <ListItemIcon>
+              <FunctionsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Summatory" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <ClearIcon />
+            </ListItemIcon>
+            <ListItemText primary="Multiplications" />
+          </ListItem>
+          <ListItem button onClick={handleClick}>
+            <ListItemIcon>
+              <ViewComfyIcon />
+            </ListItemIcon>
+            <ListItemText primary="Divisions" />
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItem button>
+                <ListItemIcon>
+                  <StarBorder />
+                </ListItemIcon>
+                <ListItemText primary="Starred" />
+              </ListItem>
+            </List>
+          </Collapse>
+        </List>
       </Drawer>
     </React.Fragment>
   );
