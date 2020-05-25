16c5113b3311f66b83b85d082dde8131e112005f
Joe Cabezas Campos <joe.cabezas@gmail.com>
2020-05-25 02:20:01 -0700

    adding production output for github pages

--- webpack.production.js
+++ webpack.production.js
@@ -1,7 +1,12 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'docs'),
+  },
 });
