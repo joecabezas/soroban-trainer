6c398490b1ffc51ff7ff78f844db554d30f0e7f0
Joe Cabezas Campos <joe.cabezas@gmail.com>
2020-05-24 17:46:45 -0700

    added appbar and drawer

--- src/app.js
+++ src/app.js
@@ -1,12 +1,51 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
+import Drawer from '@material-ui/core/Drawer';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Divider from '@material-ui/core/Divider';
 
 function App() {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <React.Fragment key={'left'}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            onClick={handleDrawerOpen}
+          >
+            <MenuIcon />
+          </IconButton
+          >
+          <Typography variant="h6">
+            Soroban Trainer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor={'left'} open={isOpen}>
+        <IconButton onClick={handleDrawerClose}>
+          <ChevronLeftIcon />
+        </IconButton>
+        <Divider />
+      </Drawer>
+    </React.Fragment>
   );
 }
 
